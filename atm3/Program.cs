b60dd02f8bc2a03b8bd22265698c49f7838bfd31
Josund04 <89245272+Josund04@users.noreply.github.com>
2023-05-02 10:09:05 +0200

    Add files via upload

---
+++ atm3/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        var atm = new Atm();
+        while (true)
+        {
+            int option;
+
+            Console.WriteLine();
+            Console.WriteLine("Meny:");
+            Console.WriteLine("1. Skapa konto");
+            Console.WriteLine("2. Insättning");
+            Console.WriteLine();
+            Console.Write("Var vänlig välj ett alternativ ");
+
+            var input = int.TryParse(Console.ReadLine(), out option);
+
+            Console.WriteLine("-----------------");
+
+            switch (option)
+            {
+                case 1:
+                    atm.CreateAccount();
+                    break;
+                case 2:
+                    atm.Deposit();
+                    break;
+            }
+        }
+    }
+}
